2b627bf0331242b96ae172e40da966d2c53d3df2
Rita <ritakuo2002@gmail.com>
2021-02-18 08:30:36 +0100

    bezig h02

---
+++ h01/opdr2.js
@@ -0,0 +1,8 @@
+var auto = {
+    merk: "Ford",
+    type: "Mondeo",
+    aantalWielen: 4,
+    kleur: "blauw"
+}
+
+console.log(auto.kleur);
---
+++ h02/opdr2_1.js
@@ -0,0 +1,3 @@
+document.getElementById("paginatitel").innerText = "Dit is de paginatiltel";
+document.getElementById("subkop").innerText = "Dit is de subkop"
+document.getElementById("img1").src = "img/"
