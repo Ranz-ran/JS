f2e49fa6e515001c92fcaf258989aa4171319aff
Rita <ritakuo2002@gmail.com>
2021-02-18 13:07:43 +0100

    h02 af

--- h02/opdr2_1.js
+++ h02/opdr2_1.js
@@ -1,3 +1,4 @@
 document.getElementById("paginatitel").innerText = "Dit is de paginatiltel";
 document.getElementById("subkop").innerText = "Dit is de subkop"
-document.getElementById("img1").src = "img/"
+document.getElementById("aap1").src = "img/baby-aap.jpg"
+document.getElementById("aap2").src = "img/app2.jpg"
